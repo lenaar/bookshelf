09f5254fa6ab2ab02e039223579b0778583728fc
Elena Rakhimova <elenara@kth.se>
2022-08-12 17:39:13 +0200

    refactor: move repetiting code to separate function

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,11 +1,17 @@
 import * as React from 'react'
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {
+  render as rtlRender,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import {queryCache} from 'react-query'
 import * as auth from 'auth-provider'
 import {buildUser, buildBook} from 'test/generate'
 import * as usersDB from 'test/data/users'
 import * as booksDB from 'test/data/books'
 import * as listItemsDB from 'test/data/list-items'
+import {formatDate} from 'utils/misc'
 import {AppProviders} from 'context'
 import {App} from 'app'
 
@@ -20,23 +26,40 @@ afterEach(async () => {
   ])
 })
 
-test('renders all the book information', async () => {
+async function render(ui, {route = '/list', user, ...renderOptions}) {
+  user = typeof user === 'undefined' ? await loginAsUser() : user
+  window.history.pushState({}, 'Test page', route)
+
+  const returnValue = {
+    ...rtlRender(<App />, {wrapper: AppProviders, ...renderOptions}),
+    user,
+  }
+  await waitForLoadingToFinish()
+  // we need to return render it otherwise there will be nothing to fetch by screen
+  return returnValue
+}
+
+async function loginAsUser() {
   const user = buildUser()
   await usersDB.create(user)
   const authUser = await usersDB.authenticate(user)
-
   window.localStorage.setItem(auth.localStorageKey, authUser.token)
+  return authUser
+}
 
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
-  window.history.pushState({}, 'Test page', route)
-
-  render(<App />, {wrapper: AppProviders})
-
-  await waitForElementToBeRemoved(() => [
+function waitForLoadingToFinish() {
+  return waitForElementToBeRemoved(() => [
     ...screen.queryAllByLabelText(/loading/i),
     ...screen.queryAllByText(/loading/i),
   ])
+}
+
+test('renders all the book information', async () => {
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+
+  // add await to render
+  await render(<App />, {route})
 
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
   expect(screen.getByText(book.author)).toBeInTheDocument()
@@ -62,4 +85,38 @@ test('renders all the book information', async () => {
   ).not.toBeInTheDocument()
   expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
   expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+})
+
+test('can create a list item for the book', async () => {
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+
+  // add await to render
+  await render(<App />, {route})
+
+  const addToListButton = screen.getByRole('button', {name: /add to list/i})
+  await userEvent.click(addToListButton)
+
+  expect(addToListButton).toBeDisabled()
+
+  await waitForLoadingToFinish()
+
+  expect(
+    screen.getByRole('button', {name: /mark as read/i}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('button', {name: /remove from list/i}),
+  ).toBeInTheDocument()
+  expect(screen.getByRole('textbox', {name: /notes/i})).toBeInTheDocument()
+
+  const startDateNode = screen.getByLabelText(/start date/i)
+  expect(startDateNode).toHaveTextContent(formatDate(Date.now()))
+
+  expect(
+    screen.queryByRole('button', {name: /add to list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
 })
